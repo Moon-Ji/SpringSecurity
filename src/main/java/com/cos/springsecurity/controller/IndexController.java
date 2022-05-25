d5ddaa9839f50fdd6a270aeff722752149eb29a4
moonjiyoon <ske0830@gmail.com>
2022-05-25 17:19:16 +0900

    security setting

--- src/main/java/com/cos/springsecurity/controller/IndexController.java
+++ src/main/java/com/cos/springsecurity/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.springsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // view를 리턴하겠다.
 public class IndexController {
@@ -13,5 +14,36 @@ public class IndexController {
         // 머스테치 기본 폴더 src/main/resources/
         // 뷰리졸버 설정: templates (prefix), .mustache (suffix) 생략가능
         return "index"; //src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소를 낚아챔 - securityConfig 파일 생성 후 작동 안함
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!";
     }
 }
